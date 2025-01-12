e8e69ce0d69f4cd7733b55b80398e144986a49e6
wacim-saidani <wassim.saidani@insat.ucar.tn>
2025-01-12 19:40:23 +0100

    edit the backend TF file

--- eks/backend.tf
+++ eks/backend.tf
@@ -6,14 +6,7 @@ terraform {
       version = "~> 5.49.0"
     }
   }
-  backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
-  }
-}
+
 
 provider "aws" {
   region  = var.aws-region
